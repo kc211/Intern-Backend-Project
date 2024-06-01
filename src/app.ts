af7389cce50e0dca131b88d4b5a780b2e6f25304
kc211 <devadeepkolla211@gmail.com>
2024-06-01 15:21:11 +0530

    feat! backend for the theatre booking is done

--- src/app.ts
+++ src/app.ts
@@ -3,18 +3,20 @@ import cors from "cors";
 import LoginRoute from './routes/LoginRoute'
 import RegisterRoute from './routes/RegRoute';
 import HomeRoute from './routes/HomeRoute';
-
+import MoviesRoute from './routes/MoviesRoute';
 
 const app: Express = express();
-const path = require('path');   
+import path from 'path';   
 app.use(express.json());
 
 app.use(cors());
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, 'public'))); //usage of static images 
+console.log(__dirname);
 app.use("/",HomeRoute);
 app.use("/",LoginRoute);
 app.use("/",RegisterRoute);
 app.use("/",HomeRoute);
+app.use("/",MoviesRoute);
 
 app.listen(8081, () => {
   console.log("server is running on PORT 8081");
---
+++ src/routes/MoviesRoute.ts
@@ -0,0 +1,23 @@
+import express, { Express, Request, Response } from "express";
+import db from "../dbconfig/dbconfig";
+const router = express.Router();
+
+ export default router.get("/shows/:id", async (req: Request, res: Response) => {
+  const { id } = req.params;
+  const data = await db("Movies").select("*").where({id});
+  console.log(data);
+  try{
+
+      if(data.length===0)
+        {
+            return res.status(404).json({message:"Movie Not Found"});
+        }
+        return res.json(data[0]);
+    }
+    catch(err)
+    {
+        console.error("error :",err);
+       return res.status(500).json({message:"Failed to fetch movie."});
+    }
+
+});
